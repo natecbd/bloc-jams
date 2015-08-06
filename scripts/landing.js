c4b325a94ef09ffddb3f969eb675526f5cb72612
Nathan <natecbd@gmail.com>
2015-08-06 12:06:30 -0700

    animated the landing page based on window size

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,12 +1,26 @@
-var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
 
-var revealPoint = function (i) {  
-    points[i].style.opacity = 1;
-    points[i].style.transform = "scaleX(1) translateY(0)";
-    points[i].style.msTransform = "scaleX(1) translateY(0)";
-    points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-}
+var animatePoints = function (points) {
+    var revealPoints = function (index) {
+        points[index].style.opacity = 1;
+        points[index].style.transform = "scaleX(1) translateY(0)";
+        points[index].style.msTransform = "scaleX(1) translateY(0)";
+        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+    };
+    for(var i = 0; i < points.length; i++) {
+        revealPoints(i);
+    }
+};
 
-for(var i = 0; i < points.length; i++) {
-    revealPoint(i);
+
+
+window.onload = function () {
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    window.addEventListener('scroll', function(event) {
+        if (pointsArray[0].getBoundingClientRect().top <= 500) {
+            animatePoints(pointsArray);
+        }
+    });
 }
